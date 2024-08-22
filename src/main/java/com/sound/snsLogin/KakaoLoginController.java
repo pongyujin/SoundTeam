849d9f62709e78a5cd581f6aa02041859ffa3e6a
smhrd20 <smhrd20@192.168.219.102>
2024-08-22 11:45:36 +0900

    김소리 다시 집나갔나... 08/22

--- src/main/java/com/sound/snsLogin/KakaoLoginController.java
+++ src/main/java/com/sound/snsLogin/KakaoLoginController.java
@@ -17,7 +17,7 @@ public class KakaoLoginController extends HttpServlet {
 
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String clientId = "fdb3f3491258b8bea723bdc70be03745"; // 애플리케이션 클라이언트 아이디값
-        String redirectURI = URLEncoder.encode("http://localhost:8081/SoundTeam/kakaocallback", "UTF-8");
+        String redirectURI = URLEncoder.encode("http://localhost:8081/ST/kakaocallback", "UTF-8");
         SecureRandom random = new SecureRandom();
         String state = new BigInteger(130, random).toString();
         String apiURL = "https://kauth.kakao.com/oauth/authorize?response_type=code";
